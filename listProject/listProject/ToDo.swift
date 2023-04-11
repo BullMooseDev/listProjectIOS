220a483b1a49b24a2a336d73b8d87dbe1eeb388f
kole ervine <koleervine@gmail.com>
2023-04-11 02:23:38 -0600

    completion of list guided project

--- listProject/listProject/ToDo.swift
+++ listProject/listProject/ToDo.swift
@@ -1,21 +1,42 @@
 import Foundation
 
-struct ToDo: Equatable {
-    var id = UUID()
+struct ToDo: Equatable, Codable {
+    let id: UUID
     var title: String
     var isComplete: Bool
     var dueDate: Date
     var notes: String?
     
+    init(title: String, isComplete: Bool, dueDate: Date,
+           notes: String?) {
+            self.id = UUID()
+            self.title = title
+            self.isComplete = isComplete
+            self.dueDate = dueDate
+            self.notes = notes
+        }
+    
     static func == (lhs: ToDo, rhs: ToDo) -> Bool {
             return lhs.id == rhs.id
         }
     
-    static func loadToDos() -> [ToDo]?  {
-        let toDo1 = ToDo(title: "title one", isComplete: false, dueDate: Date())
-        let toDo2 = ToDo(title: "title two", isComplete: false, dueDate: Date())
-        let toDo3 = ToDo(title: "title three", isComplete: false, dueDate: Date())
+    static let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    
+    static let archiveURL = documentsDirectory.appendingPathComponent("toDos").appendingPathExtension("plist")
+    
+    static func saveToDos(_ toDos: [ToDo]) {
+        let propertyListEncoder = PropertyListEncoder()
         
-        return [toDo1, toDo2, toDo3]
+        let codedTodos = try? propertyListEncoder.encode(toDos)
+        
+        try? codedTodos?.write(to: archiveURL, options: .noFileProtection)
+    }
+    
+    static func loadToDos() -> [ToDo]?  {
+        guard let codedToDos = try? Data(contentsOf: archiveURL) else { return nil }
+        
+        let propertyListDecoder = PropertyListDecoder()
+        
+        return try? propertyListDecoder.decode(Array<ToDo>.self, from: codedToDos)
     }
 }
